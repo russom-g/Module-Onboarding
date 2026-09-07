c302bc489f874bf96e73af22bef9c5768b22f9dc
russom <russom.g.meskel@gmail.cpm>
2026-09-07 22:17:01 +0100

    Implement charAt method

--- Sprint-2/1-key-exercises/2-initials.js
+++ Sprint-2/1-key-exercises/2-initials.js
@@ -5,6 +5,6 @@ const lastName = "Johnson";
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-const initials = ``;
+const initials = `${firstName.charAt()}${middleName.charAt()}${lastName.charAt()}`;
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
