a613e4ca42f269524dd3bf4fa25ae9b304338fe0
russom <russom.g.meskel@gmail.cpm>
2026-09-08 21:20:02 +0100

    Answer question. Fix code. Add comma.

--- Sprint-2/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-2/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -20,3 +20,16 @@ console.log(`The percentage change is ${percentageChange}`);
 // d) Identify all the lines that are variable declarations
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+// Answers
+
+// a) There are no function or function calls in this code.
+
+// b) The error was online 5 inside replaceAll method parentheses a comma is missing between the double quotes.
+
+// c) Line 4 carPrice. Line 5 priceAfterOneYear.
+
+// d) Line 1, 2, 7 and 8.
+
+// e) The method replaceALL is removing the comma. Then the Number method is converting the string into numbers. 
+//    The purpose is to use number operations and calculate the percentage change.
