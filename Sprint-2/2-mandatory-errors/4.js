209739c6a301b1731cc3b0f4e4baf12df26f909b
russom <russom.g.meskel@gmail.cpm>
2026-09-08 20:56:47 +0100

    Fix variable name declaration

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -1,2 +1,4 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+const twelveHourClockTime = "8:53pm";
+const twentyFourHourClockTime = "20:53";
+
+// Variable names cannot start with a number in Java Script.
