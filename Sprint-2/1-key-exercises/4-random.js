9a013e128405b8ca085d115fd454e9a32d6c481e
russom <russom.g.meskel@gmail.cpm>
2026-09-08 20:12:24 +0100

    Explain how the code works

--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -7,3 +7,10 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// 1. num is a variable that stores the final result of the expression assigned to it.
+// 2. The Math.random() method generates a random decimal number from 0 up but not excluding 1.
+// 3. The Math.floor() method round a decimal number down to a whole number.
+// 4. (maximum - minimum + 1) calculates how many possible whole numbers there are. 
+//    Java Script evaluates the expressions inside the inner parentheses first. 
+//    + minimum adds 1 to move the range from 0 to the minimum 1. 
