50b15169eb92d6429eea6d52190512159d2aef55
russom <russom.g.meskel@gmail.cpm>
2026-09-08 20:52:27 +0100

    Fix data type

--- Sprint-2/2-mandatory-errors/3.js
+++ Sprint-2/2-mandatory-errors/3.js
@@ -1,5 +1,7 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = Number(String(cardNumber).slice(-4));
+
+console.log(last4Digits);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +9,8 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// Prediction: slice() is a string method. That is why it is not working on numbers.
+// Error message: type error
+// Convert the number to string 
+// If we need to keep number we need to convert it back from string to number using Number() method
