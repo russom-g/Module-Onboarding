b3f5ce422fff3e6b9eaf4a55d3bd62d37736666e
russom <russom.g.meskel@gmail.cpm>
2026-09-11 21:50:04 +0100

    Explain code step by step.

--- Sprint-2/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-2/3-mandatory-interpret/3-to-pounds.js
@@ -1,19 +1,11 @@
 const penceString = "399p";
 
-const penceStringWithoutTrailingP = penceString.substring(
-  0,
-  penceString.length - 1
-);
+const penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1);
 
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-const pounds = paddedPenceNumberString.substring(
-  0,
-  paddedPenceNumberString.length - 2
-);
+const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);
 
-const pence = paddedPenceNumberString
-  .substring(paddedPenceNumberString.length - 2)
-  .padEnd(2, "0");
+const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
 
 console.log(`£${pounds}.${pence}`);
 
@@ -33,4 +25,16 @@ console.log(`£${pounds}.${pence}`);
 //    This way only 399 is extracted of the string. Then the result is assigned to penceStringWithoutTrailingP variable.
 
 // 3. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0"):
-//    The method padStart(3, "0") adds the string 0 before penceStringWithoutTrailingP only 
+//    The method padStart(3, "0") adds the string 0 before if the length of penceStringWithoutTrailingP string is less than 3 characters.
+//    It then assign it to paddedPenceNumberString variable. For example if the string is 11, it returns 011 or the string is 1, it returns 001.
+
+// 4. const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2)
+//    Again the method substring() returns the characters starting at length 0 and removing the last two characters which are the pence(00).
+//    This gives us only the pounds value and it the assigned to the pounds variable.
+
+// 5. const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0"):
+//    substring method first returns the last two characters of the paddedPenceNumberString string. 
+//    Then the padEnd() method adds a 0 if the length of the string is less than 2. Then it assigns it to pence variable.
+
+// 6. console.log(`£${pounds}.${pence}`): 
+//    Finally console.log prints the pounds and pence adding the the pound sign £ first using template literal as £3.99
