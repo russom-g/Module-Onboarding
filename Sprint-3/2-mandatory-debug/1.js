444757f3da435b52011c9f4c1f1c088b19c65950
russom <russom.g.meskel@gmail.cpm>
2026-09-13 11:38:46 +0100

    Predict, explain, and fix code

--- Sprint-3/2-mandatory-debug/1.js
+++ Sprint-3/2-mandatory-debug/1.js
@@ -1,6 +1,8 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+// it will return syntax error
+
 function sum(a, b) {
   return;
   a + b;
@@ -9,5 +11,16 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
-// Finally, correct the code to fix the problem
+
+// There is a semicolon after return.
+
+// Finally, correct the code to fix the problem.
+// Later I discovered having a line between return and a + b gives another undefined error message
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
