9aadee0ff09560c140928ea294b4e385c919b9fb
russom <russom.g.meskel@gmail.cpm>
2026-09-13 11:28:07 +0100

    Predict, explain and fix code

--- Sprint-3/2-mandatory-debug/0.js
+++ Sprint-3/2-mandatory-debug/0.js
@@ -2,13 +2,23 @@
 
 // =============> write your prediction here
 
-function multiply(a, b) {
+// 'The result of multiplying 10 and 32 is 10 * 32' is printed.
+
+/*function multiply(a, b) {
   console.log(a * b);
 }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);*/
 
 // =============> write your explanation here
 
+// the function prints a * b. When presented with multiply(10, 32) it prints 10 * 32.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return (a * b);
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
