1df46e8efdc17caf819dd419777dfbe4dfee9266
russom <russom.g.meskel@gmail.cpm>
2026-09-13 10:23:26 +0100

    Answer questions, explain and refactor code.

--- Sprint-3/1-key-errors/0.js
+++ Sprint-3/1-key-errors/0.js
@@ -1,6 +1,10 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+// Prediction: the method toUpperCase return the character at index 0 of str as uppercase.
+// slice method will return the characters of str starting at index 1 excluding the first character.
+// the template literal combines the converted upper case first character and the rest of the string.
+
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
@@ -10,4 +14,12 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+
+// Explanation: 1. str declares twice.Hence it gives syntax error.
+// Refactor:    2. instead of returning str return can directly send back the template literal expression. 
+
 // =============> write your new code here
+
+function capitalise(str) {
+  return `${str[0].toUpperCase()}${str.slice(1)}`;  
+}
