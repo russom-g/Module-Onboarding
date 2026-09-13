6f4e46f41b18331ec2903f232d0255d419a2b76d
russom <russom.g.meskel@gmail.cpm>
2026-09-13 11:08:31 +0100

    Predict, explain and fix code

--- Sprint-3/1-key-errors/2.js
+++ Sprint-3/1-key-errors/2.js
@@ -5,16 +5,31 @@
 
 // =============> write your prediction of the error here
 
+// Prediction:
+// 1. This will give reference error.
+// 
+
+
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
+// 
+// SyntaxError: Unexpected number
 
 // =============> explain this error message here
+
+// The function expects a parameter name and it shouldn't start with a number.
+// num should be the parameter and later 3 can be passed as an argument.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
+function square(num) {
+    return num * num;
+}
+
+console(square(3));
 
